f95ec29a09a7628eee9e2b77599eabfee764e13f
Leon Zaruvinsky <leon@lzaruvinsky.com>
2014-07-27 17:55:27 -0400

    Serves static files

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 var express = require('express');
-var app = express();
+var compress = require('compression');
 
+var app = express();
+app.use(compress());
 
 app.use(function(request, response, next) {
 	console.log('Request received for: ' + request.path);
 	next();
 });
 
+// Get all static files
+app.use('/', express.static(__dirname + '/static'));
+
 app.use('/', function(request, response) {
 	response.writeHead(200, {"Content-Type":"text/plain"});
+	response.write('Welcome to ' + request.path + '\n');
 	response.end("Hello, world!");
 });
 
