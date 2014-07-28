5ac5eb68f8a09738df461ca7b98d7d37d772dcd0
Leon Zaruvinsky <leon@lzaruvinsky.com>
2014-07-27 21:46:29 -0400

    Execute db command

--- blog.js
+++ blog.js
@@ -1,8 +1,18 @@
-var dbauth = require('./dbauth.js');
-
 function blog(req, res) {
 	res.write('You are now entering blog territory.\n');
 	res.end('View path (for use in articles): ' + req.path);
+
+	var db = require('./dbauth.js');
+	db.blog.find({"color":"red"}, function(err, records) {
+		if (err) {
+			console.log("Database error");
+			res.writeHead(500, {"Content-Type":"text/plain"});
+			res.end("500: Database Error");
+			return;
+		}
+		console.log(records);
+	}
+
 }
 
 module.exports = blog
