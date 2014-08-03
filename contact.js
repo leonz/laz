a8e38549b3234a0441c63bd2863d1266dfe7a50f
Leon Zaruvinsky <leon@lzaruvinsky.com>
2014-08-03 01:38:38 -0400

    Back-end contact form validation, fully functional with and without JS enabled, and SendGrid connected

--- contact.js
+++ contact.js
@@ -1,16 +1,95 @@
+var querystring = require('querystring');
+var sendgrid = require('./contactauth')();
+
+var isValid = function(input) {
+	var checkName = input["name"] !== "Name" && input["name"].length > 0;
+
+	var pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;				
+	var checkEmail = pattern.test(input["email"]);
+
+	var checkMessage = input["message"] !== "Your message" && input["message"].length > 0;
+
+	var checkInit = input["init"].toUpperCase() === "LZ" || input["init"].toUpperCase() === "LAZ";
+
+	return checkName && checkEmail && checkMessage && checkInit;
+};
+
 function contact(req, res) {
 	if (req.method === 'POST') {
-		res.write("Form sent! Though not successfully, I haven't built that part yet :)\n\n");
-		res.end("Reach me at leon@lzaruvinsky.com");
+		var data = '';
+		req.on('data', function(datum) {
+			data += datum;
+		});
+		req.on('end', function() {
+			var input = querystring.parse(data);
+
+			if (isValid(input)) {
+				// all data is valid - send!
+				sendgrid.send({
+					to: 'leon@lzaruvinsky.com',
+					fromname: input["name"],
+					from: input["email"],
+					subject: "Contact Form Response",
+					text: input["message"]
+				}, function(err, json) {
+
+					console.log("Email sent via Sendgrid: " + json);
+
+					if (err) {
+						res.end("There was an error.");
+						console.log("Error: + " + err);
+						return;
+					} 
+
+					res.render('layout', {
+						title: "Contact Me",
+						adj: "pages",
+						partials: {
+							header: "header",
+							main: "contact-success"
+						}
+					});
+				});
+
+			} else {
+				// something went wrong
+				res.render('layout', {
+					title: "Contact Me",
+					adj: "pages",
+					name: input["name"],
+					email: input["email"],
+					message: input["message"],
+					init: input["init"],
+					error: "Oops!  Looks like something went wrong.  Please check to make sure all of the fields above are filled in and correct.",
+					partials: {
+						header: "header",
+						main: "contact",
+						script: "contact-script"
+					}
+				});
+			}
+						
+
+
+
+ 
+		//	res.write("Form sent! Though not successfully, I haven't built that part yet :)\n\n");
+		//	res.end("Reach me at leon@lzaruvinsky.com");
+		});
+
 
 	} else {
 		res.render('layout', {
 			title: "Contact Me",
 			adj: "pages",
+			name: "Name",
+			email: "Email",
+			message: "Your message",
+			init: "What are my initials?",
 			partials: {
 				header: "header",
 				main: "contact",
-			script: "contact-script"
+				script: "contact-script"
 			}
 		});
 	}
--- static/validator.js
+++ static/validator.js
@@ -50,7 +50,7 @@
 	email.addEventListener('blur', function() {
 		addDefault(email, 'Email');
 		checkEmail = validate(email, function (str) {
-			var pattern = new RegExp("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+			var pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;		
 			return pattern.test(str);
 		});
 	}, false);
@@ -79,6 +79,18 @@
 		if (checkName && checkEmail && checkMessage && checkInit) {
 			// All clear, send!
 		} else {
+			if (name.value === 'Name') {
+				name.classList.add('invalid');
+			}
+			if (email.value === 'Email') {
+				email.classList.add('invalid');
+			}
+			if (message.value === 'Your message') {
+				message.classList.add('invalid');
+			}
+			if (init.value === 'What are my initials?') {
+				init.classList.add('invalid');
+			}
 			document.getElementById('error').innerHTML = "Oops! It looks like something is wrong.  Try fixing the red boxes.";
 			e.preventDefault();
 		}
